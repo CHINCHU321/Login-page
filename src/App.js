481ebf9d15b0fa6608fee78829b2b49314c115e2
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-03 19:04:58 +0530

    COMMIT

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Login from './component/Login';
+import Signup from './component/Signup';
+import { BrowserRouter as Router,
+Routes,
+Route,
+redirect, 
+} from 'react-router-dom';
+import Signup from './component/Signup';
 function App() {
   return (
     <div>
-        <Login/>
+      <Router>
+        <Routes>
+          <Route exact path='./' Component={Login}/>
+          <Route exact path='./Signup' Component={Signup}/>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
