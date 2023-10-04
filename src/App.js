39efd8d5da45c0fba9f71553d814a2378d2b57ed
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-04 16:27:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Login from './component/Login';
-import Signup from './component/Signup';
-import { BrowserRouter as Router,
-Routes,
+{/*import Signup from './component/Signup';
+
+import { BrowserRouter as Router, Routes,
 Route,
 redirect, 
-} from 'react-router-dom';
-import Signup from './component/Signup';
+} from 'react-router-dom';*/}
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route exact path='./' Component={Login}/>
-          <Route exact path='./Signup' Component={Signup}/>
-
-        </Routes>
+    {/*}  <Router>
+      <Routes>
+        <Route  exact path ="/"   Component={<Login/>} />
+        <Route exact path ="/Signup" Component={<Signup />} />
+      </Routes>
       </Router>
+  */}<Login/>
     </div>
   );
 }
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -4,19 +4,20 @@ function Login() {
   return (
     <div className="App">
         <div className="logo-box">
-        <img src="./logo.png" alt="twitter" className="logo" />
-        <h2>Sign in to Twitter</h2>
+        <svg viewBox="0 0 24 24" aria-label="X" role="img" class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-16y2uox r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
+        <h2>Sign in to X</h2>
         <button>
         <img src="./google.png" alt="google" />
         Sign in with Google
         </button>
         <button>
         <img src="./apple.png" alt="apple" />
-        sign in with Apple </button>
+        Sign in with Apple
+         </button>
         <hr></hr> 
         <span>Or</span>
         <form>
-        <input type="text" placeholder="Phone email or username" />
+        <input type="text" placeholder="Phone, email address or username" />
         <button>Next</button>
       </form>
         <button>Forgot password?</button>
